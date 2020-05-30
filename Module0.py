b2b8455509e464aec09c4e079bf8767b924f9136
Jeffrey Liew <jliew626@utexas.edu>
2020-05-30 02:06:09 -0500

    Create Module0.py

---
+++ Module0.py
@@ -0,0 +1,24 @@
+'''
+Hello CCE Member!
+    Thank you for your interest in learning about Python! May this module
+    help you learn the basics of Python and become familiar with coding.
+    We picked Python because it is a popular coding program and is easy to learn.
+    Please take your time to reach out to our project manager if you need help
+    with any coding!
+
+    Below is a test program for you to play around with. Enjoy!
+
+    God bless you all,
+    Jeffrey Liew
+    President of CCE 2020
+'''
+
+# Hashtags are used to comment in your coding
+#   It is important to comment to clarify your coding and what you did
+# Previously, I used 3 quotation marks to start and end my comment
+#   That is used if I know I will write multiple lines to comment
+#   This can be useful if you want to describe a function :)
+
+name = input("\nEnter your name: ") # Asks for user input
+                                    # \n is used to create a new line
+print(name, "is beginning to learn Python for the Kingdom of God. \n") # Prints to text
