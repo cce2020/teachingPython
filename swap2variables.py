e2f4bf9c228efbbfc5d9e865bc12e565381b39fb
Jeffrey Liew <jliew626@utexas.edu>
2020-05-31 01:14:33 -0500

    Created Model
    I created another file without the answers  so that any students can download the original task to complete on their own.
    
    I have also added some comments and gave proper credit :)

---
+++ swap2variables.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+# Author: Joon Lee
+# Date Created: May 30th, 2020
+# Editor: Jexy Liew
+###############################################################################
+# When doing the Python modules, please do the problem on your own
+#   before looking at the answer key. Feel free to use web (Stack Exchange
+#   is a reliable source) or reach out to the project manager; Essentially,
+#   the goal of these modules are to give you a small goal to help understand
+#   Python by doing your personal research on something specific.
+###############################################################################
+
+# Swap the values of the two variables
+v1 = "God is good."
+v2 = "All the time."
+print("\nIntially, we have the phrase:\n", v1, v2)
+print("We will know swap the variables.")
+
+# We want you to swap variables v1 and v2 such that the text
+#   prints the following:
+# "Now, the phrase is:
+#  All the time. Good is good,"
+
+# Write your answer below:
