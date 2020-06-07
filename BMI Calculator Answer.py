2803f84405e44fe9b18ef363ece64917bd441430
JuruJoon <61125874+JuruJoon@users.noreply.github.com>
2020-06-07 12:45:39 -0500

    Add files via upload

---
+++ BMI Calculator Answer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Author: Joon Lee
+# Date Created: 6/4/20
+###############################################################################
+# When doing the Python modules, please do the problem on your own
+#   before looking at the answer key. Feel free to use web (Stack Exchange
+#   is a reliable source) or reach out to the project manager; Essentially,
+#   the goal of these modules are to give you a small goal to help understand
+#   Python by doing your personal research on something specific.
+##############################################################################
+
+#Questions
+#Body Mass Index (BMI) is a measure that uses person's mass and height to work out if one's weight is healthy.
+#Make a BMI calculator using an if-else statement to measure if one's overweight or not.
+#Hints
+#1. unit for BMI is kg/m^2
+#2. BMI calculation divides an adult's weight by their height squared
+#3. usually BMI less than 25 is not overweight and the other is overweight
+
+#Answer
+#name = "Joon"
+#height = 1.77
+#weight = 73
+#bmi = weight/(height**2)
+#print("bmi: ")
+#print(bmi)
+#if bmi<25:
+#    print(name)
+#    print("is not overweight")
+#else:
+#    print(name)
+#    print("is overweight")
+
---
+++ BMI Calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Author: Joon Lee
+# Date Created: 6/4/20
+###############################################################################
+# When doing the Python modules, please do the problem on your own
+#   before looking at the answer key. Feel free to use web (Stack Exchange
+#   is a reliable source) or reach out to the project manager; Essentially,
+#   the goal of these modules are to give you a small goal to help understand
+#   Python by doing your personal research on something specific.
+##############################################################################
+
+#Questions
+#Body Mass Index (BMI) is a measure that uses person's mass and height to work out if one's weight is healthy.
+#Make a BMI calculator using an if-else statement to measure if one's overweight or not.
+#Hints
+#1. unit for BMI is kg/m^2
+#2. BMI calculation divides an adult's weight by their height squared
+#3. usually BMI less than 25 is not overweight and the other is overweight
+
