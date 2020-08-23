42f630fee1201bbcb79298357cf303a2fa9bb57d
Brian Kim <briankim31415@gmail.com>
2020-08-23 23:34:38 +0000

    Module 2

--- Beginner Modules/Module2_Variables.py
+++ Beginner Modules/Module2_Variables.py
@@ -1,11 +1,51 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Variables are used to store values.
+# To declare a variable in Python, assign a value to a name.
+# When coming up with a name, use something that is
+# relevant to the use case of the variable.
+# For example:
 
-# In[ ]:
+myVar = 5
 
-# Author: Joon Lee
-# Date Created: May 30th, 2020
-# Editor: Jexy Liew
+# The variable myVar now holds the data type integer (int) value '5'.
+# An int is any non-decimal number.
+# Here are some other data types:
+
+myFloat = 20.5	# A float is any decimal number.
+myBool = True	# A boolean is either true or false.
+myString = "This is a string"	# A string can contain any type and number of characters.
+myList = ["string1", "string2", "string3"]	# A list contains a set of variables of the same data type.
+
+# In Python, a variable can change to any data type.
+
+myVar = False
+
+# The variable myVar is now a boolean.
+# You can also assign variables to other variables.
+
+myVar = myString
+
+# The variable myVar is now a string that contains whatever
+# myString contains which is "This is a string".
+# To print a string, use the print() function with the variable as the parameter:
+
+print(myVar)	# Output: This is a string
+
+# To print multiple variables in one print statement, separate the variables with a + sign.
+
+print(myVar + myString)	# Output: This is a stringThis is a string
+
+# To print multiple variables with a single space in between, separate the variables with a comma.
+
+print(myVar,myString)	# Output: This is a string This is a string
+
+# Other strings can be manually added to the print statement using the + sign.
+
+print(myVar + ". This is a string.")	# Output: This is a string. This is a string.
+
+# Printing variables will become one of the easiest and
+# fastest ways to debug and find problems in your code.
+
+
 ###############################################################################
 # When doing the Python modules, please do the problem on your own
 #   before looking at the answer key. Feel free to use web (Stack Exchange
@@ -14,15 +54,32 @@
 #   Python by doing your personal research on something specific.
 ###############################################################################
 
-# Swap the values of the two variables
-v1 = "God is good."
-v2 = "All the time."
-print("\nIntially, we have the phrase:\n", v1, v2)
-print("We will now swap the variables.")
 
-# We want you to swap variables v1 and v2 such that the text
-#   prints the following:
-# "Now, the phrase is:
-#  All the time. Good is good,"
+# To test your understanding of variables, solve the following problem:
 
-# Write your answer below:
+firstLine = "God is good."
+secondLine = "All the time."
+
+# 1. Print the following variables in 2 lines using 1 print statement.
+#### Answer 1 here ####
+
+
+
+#### Answer 1 here ####
+
+# 2. Swap the 2 variables by assigning variables to other variables. You may create new variables.
+# 	 DO NOT directly assign a string to the variable (i.e. firstLine = "All the time.")
+#### Answer 2 here ####
+
+
+
+#### Answer 2 here ####
+
+# 3. Print the following variables in 1 line with proper spacing between sentences and 1 print statement.
+#### Answer 3 here ####
+
+
+
+#### Answer 3 here ####
+
+# Check your answers with the answer key to see if you did it correctly!
